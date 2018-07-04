ecfff91c1d80822ce80fb2cc3f944da389fe2081
Gustavo Calixto <gustavo.calixtosouza@gmail.com>
2018-07-04 00:13:00 -0300

    -Migrations.

--- database/migrations/2018_07_04_030608_create_measures_table.php
+++ database/migrations/2018_07_04_030608_create_measures_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMeasuresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('measures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('decibels',3,2);
+            $table->integer('id_robo');
+            $table->float('points',4,8);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('measures');
     }
 }
