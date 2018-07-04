8626338c0d3a4e2455f6938452ebbd5af855d31b
Giovanni Cavallari <jobacavallari@hotmail.com>
2018-07-04 13:20:29 -0300

    Alteração na migration users
    Coloquei o email para login no site e deixei o password como text por causa do Hash.

--- database/migrations/2018_07_04_030202_create_users_table.php
+++ database/migrations/2018_07_04_030202_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 150);
+            $table->string('email', 150)->unique();
             $table->string('rfid_tag');
-            $table->string('username');
-            $table->string('password');
+            $table->text('password');
             $table->timestamps();
         });
     }
