74d60242bf325712fb6d93e4a26d3ca2a67e37ba
Gustavo Calixto <gustavo.calixtosouza@gmail.com>
2018-07-04 22:49:22 -0300

    -Correção de conflito merge.

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-<<<<<<< HEAD
-Route::get('/', function () {
-
-	$users = DB::table('users')->get();
-
-    return view('welcome');
-});
-=======
 Auth::routes();
 
 //paginas common
 Route::get('/', 'BeginController@index')->name('welcome');
 Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> 6e515f8c4e63c06536a6465d4bfb87a37f066a40
